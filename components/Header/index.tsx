b4d49ce50810bb55f8cf8a9c30603081838277e6
Dmitry Shumskiy <ejedima260@gmail.com>
2024-02-12 14:44:37 +0200

    some markups code addded

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,25 @@
-
-
+import "./style.css";
 
 const Header:React.FC = () => {
-  return <header>Header</header>;
+  return <header>
+    <div className="container d-flex">
+      <ul className="header__nav">
+        <li>Новини</li>
+        <li>Музика</li>
+        <li>Мерч</li>
+        <li>Про нас</li>
+      </ul>
+      
+      <div className="header__logo">outside</div>
+
+      <div className="header__icons-container">
+        <div className="icons-container__logo"></div>
+        <div className="icons-container__logo"></div>
+        <div className="icons-container__logo"></div>
+        <div className="icons-container__logo"></div>
+      </div>
+    </div>
+  </header>;
 }
 
 export default Header;
