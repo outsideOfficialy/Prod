b7cd6a551e3c5d1bcf53cab222462231fd6870c4
Dmitry Shumskiy <ejedima260@gmail.com>
2024-02-12 14:22:35 +0200

    some purposes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,15 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <div className="container">
+        <div>
+          <button className="btn btn-main-primary">Test</button>
+        </div>
+        <div>
+          <button className="btn btn-scroll-up"></button>
+        </div>
+      </div>
+    </>
   )
 }
