39ccb56542589312865a38e90cd7d254e4de6884
Dmitry Shumskiy <ejedima260@gmail.com>
2024-02-20 19:33:40 +0200

    small fixes + scroll btn complete

--- components/PageTemplate/index.tsx
+++ components/PageTemplate/index.tsx
@@ -2,13 +2,34 @@ import React from "react";
 
 interface PageTemplateProps {
   children: React.ReactNode | React.ReactNode[];
+  scrollUpBtn?: boolean;
 }
 
-const PageTemplate:React.FC<PageTemplateProps> = ({
-  children
+const PageTemplate: React.FC<PageTemplateProps> = ({
+  children,
+  scrollUpBtn = true
 }) => {
+  const [showScrollUp, setShowScrollUp] = React.useState(false);
+
+  React.useEffect(() => {
+    if (scrollUpBtn) {
+      window.addEventListener("scroll", () => {
+        window.scrollY >= 100 ? setShowScrollUp(true) : setShowScrollUp(false);
+        })
+    }
+  });
+
   return <main>
     {children}
+    {scrollUpBtn &&
+      <button
+        onClick={(e) => {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        }}
+        className={`btn-scroll-up material-symbols-outlined ${showScrollUp ? "visible" : ""}`}>arrow_upward</button>}
   </main>;
 }
 
