a5636f7c3c3d5193b2a31418203d8148c835facd
Jpeg <kostya.doge@gmail.com>
2024-02-20 16:55:31 +0200

    added icons pack

---
+++ src/icons/utils.tsx
@@ -0,0 +1,27 @@
+import spotify from "./Spotify.svg";
+import youtubeMusic from "./YoutubeMusic.svg";
+import soundCloud from "./SoundCloud.svg";
+import iTunes from "./iTunes.svg";
+import instagram from "./Instagram.svg";
+import tikTok from "./TikTok.svg";
+import youTube from "./YouTube.svg";
+import twitter from "./Twitter.svg";
+import telegram from "./Telegram.svg";
+import discord from "./Discord.svg";
+import twitch from "./Twitch.svg";
+
+const obj: { [key: string]: string } = {
+  youtubeMusic: youtubeMusic,
+  spotify: spotify,
+  soundCloud: soundCloud,
+  iTunes: iTunes,
+  instagram: instagram,
+  tikTok: tikTok,
+  youTube: youTube,
+  twitter: twitter,
+  telegram: telegram,
+  discord: discord,
+  twitch: twitch
+};
+
+export default obj;
