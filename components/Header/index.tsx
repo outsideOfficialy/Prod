5b7e4e03f71426c955572e7453044ec778f7309f
KostyaDemchenko <kostya.doge@gmail.com>
2024-02-20 19:08:36 +0200

    icons added

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,14 +6,16 @@ import "./@media.scss";
 import LinkedList from "../LinkedList";
 import { motion } from "framer-motion";
 
+import icons from "@/src/icons/utils";
+import Image from "next/image";
+
 interface PagesNavProps {
   isMobile?: boolean;
   className?: string;
-  extraLinks?: { title: string, link: string }[]
+  extraLinks?: { title: string; link: string }[];
 }
 
 const PagesNav: React.FC<PagesNavProps> = ({ extraLinks, className, isMobile = false }) => {
-
   const links = [
     {
       title: "Новини",
@@ -32,32 +34,43 @@ const PagesNav: React.FC<PagesNavProps> = ({ extraLinks, className, isMobile = f
       link: "/news"
     }
   ];
-  if (extraLinks && extraLinks.length) extraLinks.forEach(el => links.unshift(el));
+  if (extraLinks && extraLinks.length) extraLinks.forEach((el) => links.unshift(el));
 
-  return <LinkedList
-    className={!className ? "header__btns-container header__nav" : className}
-    links={links}
-  />
-}
+  return (
+    <LinkedList
+      className={!className ? "header__btns-container header__nav" : className}
+      links={links}
+    />
+  );
+};
 
 const IconsList: React.FC<{
   isMobile?: boolean;
-  className?: string
+  className?: string;
 }> = ({ className, isMobile = false }) => {
-  return <div className={!className ? "header__btns-container header__icons" : className}>
-    {isMobile && <p>Наші соц. мережи</p>}
-    <div className="icons-container">
-      <div className="icons-container__logo"></div>
-      <div className="icons-container__logo"></div>
-      <div className="icons-container__logo"></div>
-      <div className="icons-container__logo"></div>
+  return (
+    <div className={!className ? "header__btns-container header__icons" : className}>
+      {isMobile && <p>Наші соц. мережи</p>}
+      <div className="icons-container">
+        {/* Используем иконки из объекта icons */}
+        <div className="icons-container__logo">
+          <Image src={icons.spotify} alt="Spotify" />
+        </div>
+        <div className="icons-container__logo">
+          <Image src={icons.iTunes} alt="iTunes" />
+        </div>
+        <div className="icons-container__logo">
+          <Image src={icons.soundCloud} alt="SoundCloud" />
+        </div>
+        <div className="icons-container__logo">
+          <Image src={icons.instagram} alt="Instagram" />
+        </div>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-const Header: React.FC<{ isMobile: boolean }> = ({
-  isMobile
-}) => {
+const Header: React.FC<{ isMobile: boolean }> = ({ isMobile }) => {
   const swipeRef = React.useRef<null | number>(null);
 
   const [sideMenuIsOpened, setSideMenuIsOpened] = React.useState(false);
@@ -68,85 +81,107 @@ const Header: React.FC<{ isMobile: boolean }> = ({
 
   const toggleSideMenuOpen = () => {
     setSideMenuIsOpened(!sideMenuIsOpened);
-  }
+  };
 
   const touchStart: React.TouchEventHandler<HTMLDivElement> = (e) => {
     swipeRef.current = e.touches[0].clientX;
-  }
+  };
 
   const touchEnd: React.TouchEventHandler<HTMLDivElement> = (e) => {
     if (!swipeRef.current) return;
     const endX = e.changedTouches[0].clientX;
     if (swipeRef.current - endX > 50) toggleSideMenuOpen();
     swipeRef.current = null;
-  }
+  };
   // search for ANIM to find my purposes for animations
 
-  return <motion.header
-    // ANIM
-    initial={{ opacity: 0, translateY: -50 }}
-    animate={{ opacity: 1, translateY: 0 }}
-    transition={{ duration: .3 }}
-    className="header">
-    {/* на мобилке сделать выпадающее меню */}
+  return (
+    <motion.header
+      // ANIM
+      initial={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ duration: 0.3 }}
+      className="header"
+    >
+      {/* на мобилке сделать выпадающее меню */}
 
-    {isMobile &&
-      <div
-        onClick={(e) => {
-          const clickedEl = e.target as HTMLDivElement;
-          if (clickedEl.classList.contains("side-menu-backdrop")) toggleSideMenuOpen();
-        }} className={`side-menu-backdrop ${sideMenuIsOpened ? "opened" : ""}`}>
+      {isMobile && (
         <div
-          onTouchStart={touchStart}
-          onTouchEnd={touchEnd}
-          className={`header__side-menu ${sideMenuIsOpened ? "opened" : ""}`}>
+          onClick={(e) => {
+            const clickedEl = e.target as HTMLDivElement;
+            if (clickedEl.classList.contains("side-menu-backdrop")) toggleSideMenuOpen();
+          }}
+          className={`side-menu-backdrop ${sideMenuIsOpened ? "opened" : ""}`}
+        >
+          <div
+            onTouchStart={touchStart}
+            onTouchEnd={touchEnd}
+            className={`header__side-menu ${sideMenuIsOpened ? "opened" : ""}`}
+          >
+            <div className="header__side-menu-container">
+              <div className="header__side-menu-title">
+                <p>МЕНЮ</p>
+                <button
+                  onClick={toggleSideMenuOpen}
+                  className="header__side-menu-close btn-close material-symbols-outlined"
+                >
+                  close
+                </button>
+              </div>
 
-          <div className="header__side-menu-container">
-            <div className="header__side-menu-title">
-              <p>МЕНЮ</p>
-              <button onClick={toggleSideMenuOpen} className="header__side-menu-close btn-close material-symbols-outlined">close</button>
+              <PagesNav />
+
+              <IconsList isMobile />
             </div>
+            <div className="header__side-menu-copyrights">
+              <a className="copyrights__mail" href="mailto:outsideoffficial@gmail.com">
+                <span className="material-symbols-outlined">mail</span>outsideoffficial@gmail.com
+              </a>
 
-            <PagesNav />
-
-            <IconsList isMobile />
-          </div>
-          <div className="header__side-menu-copyrights">
-            <a className="copyrights__mail" href="mailto:outsideoffficial@gmail.com"><span className="material-symbols-outlined">mail</span>outsideoffficial@gmail.com</a>
-
-            <p>
-              <a href="#">Terms of Service</a>
-              <a href="#">Privacy Policy</a>
-            </p>
+              <p>
+                <a href="#">Terms of Service</a>
+                <a href="#">Privacy Policy</a>
+              </p>
+            </div>
           </div>
         </div>
+      )}
+
+      <div className="container">
+        {isMobile && (
+          <button
+            className="header__side-menu-btn material-symbols-outlined"
+            onClick={toggleSideMenuOpen}
+          >
+            menu
+          </button>
+        )}
+
+        {/* если не мобилка, то отобразить иконки и навигацию на ряду с лого */}
+        {!isMobile && <PagesNav />}
+
+        <motion.h1
+          // ANIM
+          // initial={{ opacity: 0, translateY: -50 }}
+          // animate={{ opacity: 1, translateY: 0 }}
+          // transition={{ duration: 0.5 }}
+          className="header__logo"
+        >
+          outside
+        </motion.h1>
+
+        {!isMobile && <IconsList />}
       </div>
-    }
 
-    <div className="container">
-
-      {isMobile && <button className="header__side-menu-btn material-symbols-outlined" onClick={toggleSideMenuOpen}>menu</button>}
-
-      {/* если не мобилка, то отобразить иконки и навигацию на ряду с лого */}
-      {!isMobile && <PagesNav />}
-
-      <motion.h1
+      <motion.hr
         // ANIM
-        // initial={{ opacity: 0, translateY: -50 }}
-        // animate={{ opacity: 1, translateY: 0 }}
-        // transition={{ duration: 0.5 }}
-        className="header__logo">outside</motion.h1>
-
-      {!isMobile && <IconsList />}
-    </div>
-
-    <motion.hr
-      // ANIM 
-      initial={{ opacity: 0, width: 0 }}
-      animate={{ opacity: 1, width: "100%" }}
-      transition={{ duration: .5, delay: .3 }}
-      className="separate-line" />
-  </motion.header>;
-}
+        initial={{ opacity: 0, width: 0 }}
+        animate={{ opacity: 1, width: "100%" }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="separate-line"
+      />
+    </motion.header>
+  );
+};
 
 export { Header, PagesNav, IconsList };
--- src/icons/utils.tsx
+++ src/icons/utils.tsx
@@ -1,14 +1,14 @@
-import spotify from "./Spotify.svg";
-import youtubeMusic from "./YoutubeMusic.svg";
-import soundCloud from "./SoundCloud.svg";
-import iTunes from "./iTunes.svg";
-import instagram from "./Instagram.svg";
-import tikTok from "./TikTok.svg";
-import youTube from "./YouTube.svg";
-import twitter from "./Twitter.svg";
-import telegram from "./Telegram.svg";
-import discord from "./Discord.svg";
-import twitch from "./Twitch.svg";
+import spotify from "/public/icons/Spotify.svg";
+import youtubeMusic from "/public/icons/YoutubeMusic.svg";
+import soundCloud from "/public/icons/SoundCloud.svg";
+import iTunes from "/public/icons/iTunes.svg";
+import instagram from "/public/icons/Instagram.svg";
+import tikTok from "/public/icons/TikTok.svg";
+import youTube from "/public/icons/YouTube.svg";
+import twitter from "/public/icons/Twitter.svg";
+import telegram from "/public/icons/Telegram.svg";
+import discord from "/public/icons/Discord.svg";
+import twitch from "/public/icons/Twitch.svg";
 
 const obj: { [key: string]: string } = {
   youtubeMusic: youtubeMusic,
