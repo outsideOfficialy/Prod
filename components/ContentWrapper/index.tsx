e34eac11632a3660ed9d402b7ace342693d889d2
Dmitry Shumskiy <ejedima260@gmail.com>
2024-02-28 19:08:26 +0200

    some purposes with id secret

--- components/ContentWrapper/index.tsx
+++ components/ContentWrapper/index.tsx
@@ -1,13 +1,19 @@
+import "./style.scss";
 
 interface ContentWrapperProps {
   className?: string;
   children: React.ReactNode | React.ReactNode[];
+  onMouseDown?: (event: React.MouseEvent<HTMLDivElement>) => void;
+  onMouseMove?: (event: React.MouseEvent<HTMLDivElement>) => void;
+  onMouseUp?: (event: React.MouseEvent<HTMLDivElement>) => void;
 }
 
 const ContentWrapper: React.FC<ContentWrapperProps> = ({
-  className, children
+  className, children, onMouseDown, onMouseMove, onMouseUp
 }) => {
-  return <div className={`content-wrapper ${className ? className : ""}`}></div>;
+  return <div onMouseUp={onMouseUp} onMouseDown={onMouseDown} onMouseMove={onMouseMove} className={`content-wrapper ${className ? className : ""}`}>
+    {children}
+  </div>;
 }
 
 export default ContentWrapper;
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -6,16 +6,77 @@ import UnderlinedTitle from "@/components/UnderlinedTitle";
 import ContentWrapper from "@/components/ContentWrapper";
 
 import "./style.scss";
+import { SERVER_ROOT } from "@/utils/variables";
+
+interface musicRelease {
+  id: string;
+  music_type: string;
+  preview_picture: string;
+  release_name: string;
+  release_songs: string;
+  send_later: string;
+  social_media_links: string;
+}
 
 const Page: React.FC = () => {
-  return <PageLayout>
-    <div className="container">
-      <UnderlinedTitle title="Музикальні релізи" />
+  const [releasesData, setRealeasesData] = React.useState<null | musicRelease[]>(null);
+  const [showId, setShowId] = React.useState<number | null>(null);
+  const dragging = React.useRef(false);
+  const moveCoorX = React.useRef(0);
 
-      <ContentWrapper>
-        test
-      </ContentWrapper>
-    </div>
+  React.useEffect(() => {
+    fetch(`${SERVER_ROOT}/music/`)
+      .then(d => {
+        if (!d.ok) {
+          console.error("Error in fetch!", d);
+        }
+        return d.json();
+      })
+      .then(d => setRealeasesData(d))
+      .catch((reason) => console.log(reason));
+  }, []);
+
+  return <PageLayout>
+    <section className="music-releases">
+      <div className="container">
+        <UnderlinedTitle title="Музикальні релізи" />
+
+        {releasesData &&
+          <div className="music-releases__container">
+            {releasesData.map((el, idx) => {
+
+              return <ContentWrapper
+                onMouseDown={(e) => {
+                  dragging.current = true;
+                  moveCoorX.current = e.clientX;
+                }}
+                onMouseMove={(e) => {
+                  if (dragging.current == true) {
+                    const currentX = e.clientX;
+                    const deltaX = moveCoorX.current - currentX;
+
+                    if (deltaX >= 300) {
+                      // console.log('Пользователь провел мышью влево на 100 пикселей!');
+                      setShowId(idx);
+                      dragging.current = false;
+                    }
+                  }
+                }}
+                onMouseUp={(e) => dragging.current = false}
+                key={idx} className="release">
+                {showId == idx ? el.id : null}
+                <div className="release-info">
+
+                </div>
+                <div className="release-img">
+                  {/* <img src={SERVER_ROOT + "/" + JSON.parse(el.preview_picture)[0]} alt="Release Preview" /> */}
+                </div>
+              </ContentWrapper>
+            })}
+          </div>
+        }
+      </div>
+    </section>
   </PageLayout>
 }
 
