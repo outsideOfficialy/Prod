44a323d8abb81764865ddb225be3278454ef1641
Dmitry Shumskiy <ejedima260@gmail.com>
2024-02-29 13:07:09 +0200

    fixes

--- components/UnderlinedTitle/index.tsx
+++ components/UnderlinedTitle/index.tsx
@@ -3,20 +3,20 @@ import React from "react";
 
 
 
-const UnderlinedTitle: React.FC<{ title: string }> = ({
-  title
+const UnderlinedTitle: React.FC<{ title: string, subtitle?: boolean }> = ({
+  title, subtitle = false
 }) => {
   return <motion.h3
     initial={{ opacity: 0, translateY: -50 }}
     whileInView={{ opacity: 1, translateY: 0 }}
     transition={{ duration: .3, delay: .5 }}
     viewport={{ once: true }}
-    className="underlined-title">
+    className={`underlined-${subtitle ? "subtitle" : "title"}`}>
     {title}
     <motion.hr
       initial={{ width: 0 }}
       animate={{ width: "100%" }}
-      transition={{ duration: 3, delay: .5 }}
+      transition={{ duration: .3, delay: .8 }}
     />
   </motion.h3>;
 }
