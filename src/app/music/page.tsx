44bf54412a6f0e0ec74991f8952c56445179cc0a
Dmitry Shumskiy <ejedima260@gmail.com>
2024-03-01 20:56:35 +0200

    fixes

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -5,6 +5,7 @@ import PageLayout from "@/components/PageLayout";
 import UnderlinedTitle from "@/components/UnderlinedTitle";
 import ContentWrapper from "@/components/ContentWrapper";
 import IconsLinkedList from "@/components/IconsLinkedList";
+import ReactPaginate from 'react-paginate';
 
 import "./style.scss";
 import "./@media.scss";
@@ -20,9 +21,59 @@ interface musicReleaseObj {
   social_media_links: string;
 }
 
-const Page: React.FC = () => {
-  const [releasesData, setRealeasesData] = React.useState<null | musicReleaseObj[]>(null);
+interface MusicReleaseElementProps {
+  el: musicReleaseObj;
+  idx: number;
+}
+
+const MusicReleaseElement: React.FC<MusicReleaseElementProps> = ({
+  el, idx
+}) => {
   const [showId, setShowId] = React.useState<number | null>(null);
+
+  const formatedDate = el.send_later.replaceAll("/", "-");
+  const targetDate = new Date(formatedDate);
+  const currentDate = new Date();
+
+  targetDate.setHours(0, 0, 0, 0);
+  currentDate.setHours(0, 0, 0, 0);
+
+  if (currentDate.getTime() > targetDate.getTime()) return <></>;
+
+  return (
+    <ContentWrapper
+      key={idx}
+      className="music-release"
+      id={idx!}
+      setShowId={setShowId}
+    >
+      {showId == idx ? <p className="hidden-id">{el.id}</p> : null}
+
+      <div className="music-release__img-container">
+        <img src={SERVER_ROOT + "/" + JSON.parse(el.preview_picture)[0]} alt="Release Preview" />
+      </div>
+
+      <div className="release-info">
+        <div className="flex-container gap-30">
+          <UnderlinedTitle subtitle title={`${el.release_name} (${el.music_type === "single" ? "Сінгл" : "Альбом"})`} />
+          <ol className="release-info__songs-list">
+            {JSON.parse(el.release_songs).map((el: string, idx: number) => {
+              return <li key={idx + el}>{el}</li>;
+            })}
+          </ol>
+        </div>
+
+        <div className="flex-container gap-30">
+          <UnderlinedTitle subtitle title="Слухати" />
+          <IconsLinkedList buttonText="Слухати зараз" iconsList={JSON.parse(el.social_media_links)} />
+        </div>
+      </div>
+    </ContentWrapper>
+  );
+}
+
+const Page: React.FC = () => {
+  const [releasesData, setReleasesData] = React.useState<null | musicReleaseObj[]>(null);
 
   React.useEffect(() => {
     fetch(`${SERVER_ROOT}/music/`)
@@ -32,7 +83,9 @@ const Page: React.FC = () => {
         }
         return d.json();
       })
-      .then(d => setRealeasesData(d))
+      .then((d: musicReleaseObj[]) => {
+        setReleasesData(d);
+      })
       .catch((reason) => console.log(reason));
   }, []);
 
@@ -42,46 +95,7 @@ const Page: React.FC = () => {
 
       {releasesData && <>
         {releasesData.map((el, idx) => {
-          const formatedDate = el.send_later.replaceAll("/", "-");
-          const targetDate = new Date(formatedDate);
-          const currentDate = new Date();
-
-          targetDate.setHours(0, 0, 0, 0);
-          currentDate.setHours(0, 0, 0, 0);
-
-          if (currentDate.getTime() > targetDate.getTime()) return <></>;
-
-          return (
-            <ContentWrapper
-              key={idx}
-              className="music-release"
-              id={idx}
-              setShowId={setShowId}
-            >
-              {showId == idx ? <p className="hidden-id">{el.id}</p> : null}
-
-              <div className="music-release__img-container">
-                <img src={SERVER_ROOT + "/" + JSON.parse(el.preview_picture)[0]} alt="Release Preview" />
-              </div>
-
-              <div className="release-info">
-                <div className="flex-container gap-30">
-                  <UnderlinedTitle subtitle title={`${el.release_name} (${el.music_type === "single" ? "Сінгл" : "Альбом"})`} />
-                  <ol className="release-info__songs-list">
-                    {JSON.parse(el.release_songs).map((el: string, idx: number) => {
-                      return <li key={idx + el}>{el}</li>;
-                    })}
-                  </ol>
-
-                </div>
-
-                <div className="flex-container gap-30">
-                  <UnderlinedTitle subtitle title="Слухати" />
-                  <IconsLinkedList buttonText="Слухати зараз" iconsList={JSON.parse(el.social_media_links)} />
-                </div>
-              </div>
-            </ContentWrapper>
-          )
+          return <MusicReleaseElement el={el} idx={idx} />
         })}
       </>}
     </div>
